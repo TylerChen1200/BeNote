cf5562e8a0e4d4af61240e44b9f4c782cd824a00
AnanyaT129 <AnaTadig@gmail.com>
2024-12-07 14:00:03 -0500

    add profile likes table

--- BeNote/Note Full/NoteFullView.swift
+++ BeNote/Note Full/NoteFullView.swift
@@ -18,6 +18,9 @@ class NoteFullView: UIView {
     var labelCreatorReply: UILabel!
     var labelLikes: UILabel!
     var buttonLikes: UIButton!
+    var bottomAddView: UIView!
+    var buttonAdd: UIButton!
+    var tableViewLikes: UITableView!
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -27,6 +30,9 @@ class NoteFullView: UIView {
         setupContentView()
         setupLabels()
         setupLikeButton()
+        setupBottomView()
+        setupButtonAdd()
+        setupTableViewLikes()
         
         setupBackgroundImage()
         
@@ -108,6 +114,37 @@ class NoteFullView: UIView {
         buttonLikes.translatesAutoresizingMaskIntoConstraints = false
         self.addSubview(buttonLikes)
     }
+    
+    //MARK: the bottom add contact view....
+    func setupBottomView(){
+        bottomAddView = UIView()
+        bottomAddView.backgroundColor = .white
+        bottomAddView.layer.cornerRadius = 6
+        bottomAddView.layer.shadowColor = UIColor.gray.cgColor
+        bottomAddView.layer.shadowOffset = .zero
+        bottomAddView.layer.shadowRadius = 4.0
+        bottomAddView.layer.shadowOpacity = 0.7
+        bottomAddView.translatesAutoresizingMaskIntoConstraints = false
+        bottomAddView.isHidden = true
+        self.addSubview(bottomAddView)
+    }
+    
+    func setupButtonAdd(){
+        buttonAdd = UIButton(type: .system)
+        buttonAdd.titleLabel?.font = .boldSystemFont(ofSize: 16)
+        buttonAdd.setTitle("Close", for: .normal)
+        buttonAdd.translatesAutoresizingMaskIntoConstraints = false
+        bottomAddView.addSubview(buttonAdd)
+    }
+    
+    func setupTableViewLikes() {
+        tableViewLikes = UITableView()
+        tableViewLikes.register(LikesTableViewCell.self, forCellReuseIdentifier: Configs.tableViewLikesID)
+        tableViewLikes.translatesAutoresizingMaskIntoConstraints = false
+        tableViewLikes.backgroundColor = UIColor.clear
+        tableViewLikes.separatorStyle = .none
+        bottomAddView.addSubview(tableViewLikes)
+    }
 
     private func initConstraints() {
         NSLayoutConstraint.activate([
@@ -153,6 +190,21 @@ class NoteFullView: UIView {
             labelCreatorReply.leadingAnchor.constraint(equalTo: labelTimestampCreated.leadingAnchor),
             labelCreatorReply.trailingAnchor.constraint(equalTo: labelTimestampCreated.trailingAnchor),
             labelCreatorReply.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -20),
+            
+            //bottom add view...
+            bottomAddView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor,constant: -8),
+            bottomAddView.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            bottomAddView.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -8),
+            bottomAddView.topAnchor.constraint(equalTo: labelLikes.bottomAnchor, constant: 16),
+            
+            buttonAdd.bottomAnchor.constraint(equalTo: bottomAddView.bottomAnchor, constant: -8),
+            buttonAdd.leadingAnchor.constraint(equalTo: bottomAddView.leadingAnchor, constant: 4),
+            buttonAdd.trailingAnchor.constraint(equalTo: bottomAddView.trailingAnchor, constant: -4),
+            
+            tableViewLikes.topAnchor.constraint(equalTo: bottomAddView.topAnchor, constant: 8),
+            tableViewLikes.bottomAnchor.constraint(equalTo: buttonAdd.topAnchor, constant: -8),
+            tableViewLikes.leadingAnchor.constraint(equalTo: bottomAddView.leadingAnchor, constant: 4),
+            tableViewLikes.trailingAnchor.constraint(equalTo: bottomAddView.trailingAnchor, constant: -4),
         ])
     }
 
